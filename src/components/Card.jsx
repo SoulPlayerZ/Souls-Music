35bcd748003ce398bc22209b5a1e61615adbf2ad
Thyrso <thyrso.kof@gmail.com>
2022-09-24 19:21:21 -0300

    ...

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card({ album }) {
     <Link to={`/album/${album.collectionId}`} className="card" style={{width: 10 + "rem"}}>
       <section key={ album.collectionId } className="card-section">
         <img src={ album.artworkUrl100 } alt="album-front" className="card-img-top" />
-        <h4 className="card-title">{ album.collectionName }</h4>
+        <h4 className="card-header">{ album.collectionName }</h4>
         <h5 className="card-subtitle mb-2 text-muted">{ album.artistName }</h5>
       </section>
     </Link>
