aba39a5965c8f5a09686bb2ba7fd61e15925db2b
Thyrso <thyrso.kof@gmail.com>
2022-09-30 14:14:29 -0300

    feat: Estiliza track card

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 function TrackCard ({track, index}) {
-    // console.log(track);
   return(
       <label className="track-card">
         <h3>{ index + 1 + " - " + track.trackName }</h3>
-        <audio src={ track.previewUrl } controls>
-          <track kind={track.kind} />
-            O seu navegador não suporta o elemento <code>audio</code>.
-        </audio>
-        <input type="checkbox"></input>
+        <h5>{ track.artistName }</h5>
+        <section className="audio-container">
+          <audio src={ track.previewUrl } controls>
+            <track kind={track.kind} />
+              O seu navegador não suporta o elemento <code>audio</code>.
+          </audio>
+          <input type="checkbox" className="favorite-track"></input>
+        </section >
       </label>
   )
 }
