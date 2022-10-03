72ace5ab3175291a1339e069925484379b2745a0
Thyrso <thyrso.kof@gmail.com>
2022-10-03 13:59:15 -0300

    feat: Implementa rotas Profile e Favorites

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Tracks from './pages/Tracks';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/likeds" element={<Favorites />} />
         <Route path="/album/:id" element={<Tracks />} />
         <Route path="/search" element={<Search />} />
         <Route path="/" element={<Login />} />
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Profile () {
+  return(
+    <main>
+    <Header />
+    <h1>PROFILE PAGE</h1>
+  </main>
+    
+  )
+}
+
+export default Profile;
