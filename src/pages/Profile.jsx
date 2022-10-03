efde79c540fea44135383b4b89dadd8abaceaa6c
Thyrso <thyrso.kof@gmail.com>
2022-10-03 14:34:44 -0300

    feat: estiliza componente Loading

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Loading from "../components/Loading";
 
 function Profile () {
   return(
     <main>
     <Header />
+    <Loading />
     <h1>PROFILE PAGE</h1>
   </main>
     
